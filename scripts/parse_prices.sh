b85ed2b74df117e3d843aa8079fa3c4b88bab23d
Asier Martinez <asier@topxel.com>
2021-06-06 12:22:17 +0200

    New JQ parsing workflow

--- scripts/parse_prices.sh
+++ scripts/parse_prices.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sourcefile=$1
+destinationfile=./prices/${sourcefile##*/}
+
 jq '
   def cleanprice: 
     if test("\\,") then sub(","; "") 
@@ -8,52 +11,23 @@ jq '
     else . end;
 
   def replacetime:
-    .["time"] = .["Hora"][0:2] + ":00";
+    .["time"] = .["Hora"][0:2] + ":00" |
+    del(.Hora);
   
   def replacedate:
     .["date"] = 
       .["Dia"][6:10] + "-" +
       .["Dia"][3:5] + "-" +
-      .["Dia"][0:2];
-
-  def deletekeys:
-    del(.Dia, .Hora, .COF2TD, .PCB, .CYM, .PMHPCB, .PMHCYM, .SAHPCB, .SAHCYM, .FOMPCB, .FOMCYM, .FOSPCB, .FOSCYM, .INTPCB, .INTCYM, .PCAPPCB, .PCAPCYM, .TEUPCB, .TEUCYM, .CCVPCB, .CCVCYM, .EDSRPCB, .EDSRCYM);
-
-  def icbvalues:
-    .["ICB"] = {
-      price: .PCB,
-      market: .PMHPCB,
-      adjustment: .SAHPCB,
-      om_financing: .FOMPCB,
-      os_financing: .FOSPCB,
-      uninterruptibility: .INTPCB,
-      capacity: .PCAPPCB,
-      tolls: .TEUPCB,
-      variables: .CCVPCB,
-      auctions: .EDSRPCB
-    };
-
-  def camvalues: 
-    .["CAM"] = {
-      price: .CYM,
-      market: .PMHCYM,
-      adjustment: .SAHCYM,
-      om_financing: .FOMCYM,
-      os_financing: .FOSCYM,
-      uninterruptibility: .INTCYM,
-      capacity: .PCAPCYM,
-      tolls: .TEUCYM,
-      variables: .CCVCYM,
-      auctions: .EDSRCYM
-    };
+      .["Dia"][0:2] |
+    del(.Dia);
   
   [.PVPC
     | .[]
-    | map_values(cleanprice)
+    | { Dia, Hora, PCB, CYM }
     | replacedate
     | replacetime
-    | icbvalues
-    | camvalues
-    | deletekeys
+    | .["pcb_price"] = (.PCB | cleanprice)
+    | .["cym_price"] = (.CYM | cleanprice)
+    | del(.PCB, .CYM)
   ]
-' ./source/2021-06-05_Sat.json > ./prices/2021-06-05_Sat.json
+' ${sourcefile} > ${destinationfile}
