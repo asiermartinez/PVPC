dd67008beb8a01b661c3f334b000cdb16cf30a5b
Asier Martinez <asier@topxel.com>
2021-06-07 23:41:18 +0200

    Fixing download dates

--- scripts/get_prices.sh
+++ scripts/get_prices.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 FILENAME=$1
+DATE=$(TZ="Europe/Madrid" date +%F)
 
-curl -X GET \
-  "https://api.esios.ree.es/archives/70/download_json?locale=en" \
+curl -sS -X GET \
+  "https://api.esios.ree.es/archives/70/download_json?locale=en&date=${DATE}" \
 	-H "Accept: application/json; application/vnd.esios-api-v2+json" \
 	-H "Content-Type: application/json" \
 	-H "Host: api.esios.ree.es" \
